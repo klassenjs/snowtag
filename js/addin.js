c85d4eae437d70f7e34be8a27736e3c8ed3fac78
Helgeson <Nicole.Helgeson@ci.stpaul.mn.us>
2019-08-30 10:38:01 -0500

    new js

--- js/addin.js
+++ js/addin.js
@@ -21,7 +21,7 @@ geotab.addin.geotabHeatMap = function (api, state) {
 		},
 
         initializeInterface = function () {
-            baseLayer = L.tileLayer('https://{s}.tiles.mapbox.com/v3/geotab.i8d8afbp/{z}/{x}/{y}.png');
+            baseLayer = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw');
 
             heatMapLayer = L.TileLayer.heatMap({
                 radius: {
