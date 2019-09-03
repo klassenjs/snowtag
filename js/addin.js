9e84ccef8d1e077cd9e425ae82b500b6e9babe64
Helgeson <Nicole.Helgeson@ci.stpaul.mn.us>
2019-09-03 14:05:36 -0500

    js map

--- js/addin.js
+++ js/addin.js
@@ -45,7 +45,7 @@ geotab.addin.geotabHeatMap = function (api, state) {
             });
 
             map = new L.Map('addin-map', {
-                center: new L.LatLng(44.94, -93.20),
+                center: new L.LatLng(44.94, -93.10),
                 zoom: 13,
                 layers: [baseLayer, heatMapLayer]
             });
