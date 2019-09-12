b494337b7800442976e6806b65945d6efc03fec6
Helgeson <Nicole.Helgeson@ci.stpaul.mn.us>
2019-09-12 13:16:38 -0500

    api test

--- js/main.js
+++ js/main.js
@@ -60,141 +60,140 @@ console.log(7);
 console.log(deviceId);
 
 
- api.call("Get", {
-        "typeName": "ExceptionEvent",
-        "search": {
-            "deviceSearch": {
-                "id": deviceId
-            },
-            "ruleSearch": {
-                "id": "a1wrQ3PBsTUuNVZ7cqjCjHA",
-                "includeZoneStopRules": false
-            },
-            "fromDate": dateFrom,
-            "toDate": dateTo
+    api.call('Get', {
+      typeName: 'LogRecord',
+      search: {
+        'deviceSearch': {
+          id: deviceId
+        },
+        fromDate: dateFrom,
+        toDate: dateTo
       }
-    }, function(exception) {
-        for (var i = 0; i < exception.length; i++){
-            logRecord(exception[i]);
-        }
-    }
-    );
-
-function logRecord(exception) {
-    api.call("Get", {
-        "typeName": "LogRecord",
-        "search": {
-            "fromDate": exception.activeFrom,
-            "toDate": exception.activeTo,
-            "deviceSearch": {
-                "id": exception.device.id
-            }
-        }
     }, logRecords => {
+      let coordinates = [];
+      let bounds = [];
 
-      console.log(logRecords[0].longitude);
-
-      for (var x=0; x < logRecords.length; x++){
-      console.log(logRecords[x].latitude);}
-
-
-        api.call("GetAddresses", {
-            "coordinates": [{
-                "x": logRecords[0].longitude,
-                "y": logRecords[0].latitude
-            }],
-            "movingAddreses": false,
-            "hosAddresses": false
-
-        }, function(Address) {
-            api.call("Get", {
-                "typeName": "Device",
-                "search": {
-                    "id": exception.device.id
-                }
-            }, function(Device) {
-                        api.call("Get", {
-                            "typeName": "Rule",
-                            "search": {
-                                "id": exception.rule.id
-                            }
-                        }, function(Rule) {
-                            console.log(Device[0].name + " was at : " + Address[0].formattedAddress +
-                            ", (coordinates: " + logRecords[0].latitude + ", " + logRecords[0].longitude +
-                            ") and triggered the " + Rule[0].name + " rule. They were active from" + exception.activeFrom + "to" + exception.activeTo);
-                        });
-                    }
-                );
-        });
-
-        let coordinates = [];
-        let bounds = [];
-
-        for (let i = 0; i < logRecords.length; i++) {
-          if (logRecords[i].latitude !== 0 || logRecords[i].longitude !== 0) {
-
-            coordinates.push({
-              lat: logRecords[i].latitude,
-              lon: logRecords[i].longitude,
-              value: 1
-            });
-            bounds.push(new L.LatLng(logRecords[i].latitude, logRecords[i].longitude));
-
-            console.log(bounds)
-          }
+      for (let i = 0; i < logRecords.length; i++) {
+        if (logRecords[i].latitude !== 0 || logRecords[i].longitude !== 0) {
+          coordinates.push({
+            lat: logRecords[i].latitude,
+            lon: logRecords[i].longitude,
+            value: 1
+          });
+          bounds.push(new L.LatLng(logRecords[i].latitude, logRecords[i].longitude));
         }
-        if (coordinates.length > 0) {
-          map.fitBounds(bounds);
-          heatMapLayer.setLatLngs(coordinates);
-        } else {
-          errorHandler('Not enough data');
-        }
+      }
+
+      if (coordinates.length > 0) {
+        map.fitBounds(bounds);
+        heatMapLayer.setLatLngs(coordinates);
+      } else {
+        errorHandler('Not enough data to display');
+      }
+
       toggleLoading(false);
     }, error => {
       errorHandler(error);
       toggleLoading(false);
     });
-}
-};
+  };
 
-  //   api.call('Get', {
-  //     typeName: 'LogRecord',
-  //     resultsLimit: 1000,
-  //     search: {
-  //       deviceSearch: {
-  //         id: deviceId
-  //       },
-  //       fromDate: dateFrom,
-  //       toDate: dateTo
-  //     }
-  //   }, function (logRecords) {
-  //     let coordinates = [];
-  //     let bounds = [];
-  //
-  //     for (let i = 0; i < logRecords.length; i++) {
-  //       if (logRecords[i].latitude !== 0 || logRecords[i].longitude !== 0) {
-  //         coordinates.push({
-  //           lat: logRecords[i].latitude,
-  //           lon: logRecords[i].longitude,
-  //           value: 1
-  //         });
-  //         bounds.push(new L.LatLng(logRecords[i].latitude, logRecords[i].longitude));
-  //       }
-  //     }
-  //
-  //     if (coordinates.length > 0) {
-  //       map.fitBounds(bounds);
-  //       heatMapLayer.setLatLngs(coordinates);
-  //     } else {
-  //       errorHandler('Not enough data');
-  //     }
-  //
-  //     toggleLoading(false);
-  //   }, function (error) {
-  //     errorHandler(error);
-  //     toggleLoading(false);
-  //   });
-  // };
+//  api.call("Get", {
+//         "typeName": "ExceptionEvent",
+//         "search": {
+//             "deviceSearch": {
+//                 "id": deviceId
+//             },
+//             "ruleSearch": {
+//                 "id": "a1wrQ3PBsTUuNVZ7cqjCjHA",
+//                 "includeZoneStopRules": false
+//             },
+//             "fromDate": dateFrom,
+//             "toDate": dateTo
+//       }
+//     }, function(exception) {
+//         for (var i = 0; i < exception.length; i++){
+//             logRecord(exception[i]);
+//         }
+//     }
+//     );
+//
+// function logRecord(exception) {
+//     api.call("Get", {
+//         "typeName": "LogRecord",
+//         "search": {
+//             "fromDate": exception.activeFrom,
+//             "toDate": exception.activeTo,
+//             "deviceSearch": {
+//                 "id": exception.device.id
+//             }
+//         }
+//     }, logRecords => {
+//
+//       console.log(logRecords[0].longitude);
+//
+//       for (var x=0; x < logRecords.length; x++){
+//       console.log(logRecords[x].latitude);}
+//
+//
+//         api.call("GetAddresses", {
+//             "coordinates": [{
+//                 "x": logRecords[0].longitude,
+//                 "y": logRecords[0].latitude
+//             }],
+//             "movingAddreses": false,
+//             "hosAddresses": false
+//
+//         }, function(Address) {
+//             api.call("Get", {
+//                 "typeName": "Device",
+//                 "search": {
+//                     "id": exception.device.id
+//                 }
+//             }, function(Device) {
+//                         api.call("Get", {
+//                             "typeName": "Rule",
+//                             "search": {
+//                                 "id": exception.rule.id
+//                             }
+//                         }, function(Rule) {
+//                             console.log(Device[0].name + " was at : " + Address[0].formattedAddress +
+//                             ", (coordinates: " + logRecords[0].latitude + ", " + logRecords[0].longitude +
+//                             ") and triggered the " + Rule[0].name + " rule. They were active from" + exception.activeFrom + "to" + exception.activeTo);
+//                         });
+//                     }
+//                 );
+//         });
+//
+//         let coordinates = [];
+//         let bounds = [];
+//
+//         for (let i = 0; i < logRecords.length; i++) {
+//           if (logRecords[i].latitude !== 0 || logRecords[i].longitude !== 0) {
+//
+//             coordinates.push({
+//               lat: logRecords[i].latitude,
+//               lon: logRecords[i].longitude,
+//               value: 1
+//             });
+//             bounds.push(new L.LatLng(logRecords[i].latitude, logRecords[i].longitude));
+//
+//             console.log(bounds)
+//           }
+//         }
+//         if (coordinates.length > 0) {
+//           map.fitBounds(bounds);
+//           heatMapLayer.setLatLngs(coordinates);
+//         } else {
+//           errorHandler('Not enough data');
+//         }
+//       toggleLoading(false);
+//     }, error => {
+//       errorHandler(error);
+//       toggleLoading(false);
+//     });
+// }
+// };
 
   /**
    * Intialize the user interface
